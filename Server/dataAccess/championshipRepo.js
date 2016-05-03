8c6cca9877da4488378c9c69cea03850622dd4ac
Antony Durán Hernández <ajdurancr@gmail.com>
2016-05-03 02:54:10 -0600

    'updateUserPoints' method update

--- Server/dataAccess/championshipRepo.js
+++ Server/dataAccess/championshipRepo.js
@@ -11,31 +11,9 @@ exports.getUsers = function(params, callback){
 			callback(res);
 		}
 	);
-	/*callback({
-			success: true,
-			data: [
-				{user: "User1", points: 14},
-				{user: "User2", points: 15},
-				{user: "User3", points: 64},
-				{user: "User4", points: 1},
-				{user: "User5", points: 3},
-				{user: "User6", points: 554},
-				{user: "User7", points: 22},
-				{user: "User8", points: 67},
-				{user: "User9", points: 12},
-				{user: "User10", points: 8},
-				{user: "User11", points: 234},
-				{user: "User12", points: 445},
-				{user: "User13", points: 4},
-				{user: "User14", points: 3}
-			]
-		}
-	);	*/
 };
 
 exports.updateUserPoints = function(params, callback){
-    console.log('updateUserPoints: console.log(params);')
-    console.log(params);
 	var queryParams = {
 		query: { "_id": params.userId },
       		updateQuery: { $inc: { "points": params.points } },
