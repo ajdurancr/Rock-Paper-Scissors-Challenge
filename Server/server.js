9588f7accbc3081f187d9beb74e3e6126a18eb96
Antony Durán Hernández <ajdurancr@gmail.com>
2016-05-03 03:00:47 -0600

    configuration update

--- Server/server.js
+++ Server/server.js
@@ -5,7 +5,7 @@ bodyParser = require('body-parser');
 var express       = require('express'),
       app              = express(),
       server          = require('http').createServer(app),
-      port              = 8080;
+      port              = 8081;
 //-------------------------------------------------------------------------
 
 app.use(bodyParser.urlencoded({ extended: false }));
