21a9741f65da0f342acde2525e551319b74456f5
Antony Durán Hernández <ajdurancr@gmail.com>
2016-05-03 02:57:16 -0600

    'newChampionship' method update

--- Server/controllers/championshipController.js
+++ Server/controllers/championshipController.js
@@ -1,24 +1,19 @@
 var campionshipService = require('../businessLogic/championshipService');
 
 exports.newRecord = function(cRequest, cResponse) {
-  campionshipService.saveNewRecord(cRequest.body.players, function(res){
+  campionshipService.addNewRecord(cRequest.body, function(res){
         cResponse.send(res);
     });
 };
-campionshipService.saveNewRecord({first: 'tonny2'}, function(res){
-        console.log('call campionshipService.saveNewRecord: console.log(res);');
-        console.log(res);
-    });
 
 exports.getTop = function(cRequest, cResponse) {
-  //campionshipService.getTop(cRequest.params.count, function(res){
-  campionshipService.getTop(12, function(res){
+  campionshipService.getTop(cRequest.params.count, function(res){
         cResponse.send(res);
     });
 };
 
 exports.newChampionship = function(cRequest, cResponse) {
-  campionshipService.saveNewRecord(cRequest.body, function(res){
+  campionshipService.newChampionship(cRequest.body, function(res){
         cResponse.send(res);
     });
 };
