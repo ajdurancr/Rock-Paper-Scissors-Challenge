6ad4e4b324ea0f4c6ccaebb3e5b2aeb2f7e93c0e
Antony Durán <ajdurancr.dev@gmail.com>
2022-12-02 16:05:28 -0600

    update styles

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,13 @@ import { GamePlay } from '../components/GamePlay';
 import { Page as GenericPage } from '../components/Page';
 
 const Page = () => (
-  <GenericPage title='Ready to Rock-Paper-Scissors?'>      
+  <GenericPage>
+    <h1
+      className='inline bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text font-display text-4xl tracking-tight text-transparent pt-4 pb-4 mr-4 ml-4 xs:mr-auto xs:ml-auto '
+    >
+      <p className='text-3xl'>Ready to</p>
+      Rock-Paper-Scissors?
+    </h1>
     <GamePlay />
   </GenericPage>
 );
